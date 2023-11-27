aded296aa11c747619c9e1f878198f20f4ee2864
polina <polina@FE60>
2023-11-27 17:54:30 +0400

    Creating Class Vector

--- Lab1/Lab1.h
+++ Lab1/Lab1.h
@@ -1,8 +1,42 @@
-﻿// Lab1.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
+﻿#include <stdexcept>
+#include <complex> 
+#include <cmath>
 #include <iostream>
+#include <random>
+#define VECTOR_H_
 
-// TODO: Reference additional headers your program requires here.
+using namespace std;
+
+const double PI = 3.14159265358979323846;
+
+template <typename T>
+class Vector {
+private: 
+	T* _elements;
+	size_t _size;
+
+public:
+	Vector(size_t size, const T& value = T());
+	Vector(size_t size, T low_bound, T up_bound);
+
+	T& operator[](size_t index);
+	const T& operator[](size_t index) const;
+
+	Vector operator+(const Vector& other) const;
+	Vector operator-(const Vector& other) const;
+
+	T operator*(const Vector& other) const;
+	Vector operator*(const T& scalar) const;
+
+	Vector operator/(const T& scalar) const;
+
+	Vector(const Vector& other);
+	~Vector();
+
+	Vector& operator=(const Vector& other);
+	size_t Get_Dim() const;
+
+	bool operator==(const Vector& other) const;
+	bool operator!=(const Vector& other) const;
+	friend ostream& operator<<(ostream& os, const Vector& vector);
+};
