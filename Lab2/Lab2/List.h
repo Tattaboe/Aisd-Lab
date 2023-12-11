34f2e7c67dc0bcb50a8dd91ac4bc8995d913da6b
polina <polina@FE60>
2023-12-12 01:39:58 +0400

    implementation of default constructors, copy, list of random values and assignment operations

--- Lab2/Lab2/List.h
+++ Lab2/Lab2/List.h
@@ -25,3 +25,56 @@ class Cycl_List {
 public:
     Cycl_List() : _head(nullptr), _tail(nullptr), _size(0) {};
 
+    Cycl_List(const Cycl_List& other) {
+        _head = nullptr;
+        _tail = nullptr;
+        Node<T>* tmp = other._head;
+        push_tail(tmp->data);
+        tmp = tmp->next;
+        while (tmp != other._head) {
+            push_tail(tmp->data);
+            tmp = tmp->next;
+        }
+    }
+
+
+    Cycl_List(size_t size, T low_bound, T up_bound) :Cycl_List() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        if constexpr (std::is_integral_v<T>) {
+            std::uniform_int_distribution<T> dist(low_bound, up_bound);
+
+            for (size_t i = 0; i < size; ++i) {
+                T data = dist(gen);
+                push_tail(data);
+            }
+        }
+        else if constexpr (std::is_floating_point_v<T>) {
+            std::uniform_real_distribution<T> dist(low_bound, up_bound);
+
+            for (size_t i = 0; i < size; ++i) {
+                T data = dist(gen);
+                push_tail(data);
+            }
+        }
+        _size = size;
+    }
+
+
+    Cycl_List& operator=(const Cycl_List& other) {
+        if (this != &other) {
+            Cycl_List tmp(other); 
+            std::swap(_head, tmp._head); 
+            std::swap(_tail, tmp._tail); 
+            _size = tmp._size; 
+        }
+        return *this;
+    }
+
+    size_t size() const {
+        return _size;
+    }
+
+
+
