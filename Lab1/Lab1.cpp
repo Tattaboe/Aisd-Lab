af0ebaa2c39bad5fcc25afc1ad0fdeadc62576e4
polina <polina@FE60>
2023-12-11 21:49:06 +0400

    Task rework

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -7,38 +7,23 @@ using namespace std;
 
 int main()
 {
-	int size_b, size_a;
-	double value_b, value_a;
+	int  size_a;
+	double  value_a;
 	
 	
 	
 	cout << "Enter the size and value of an arbitrary vector" << endl;
 	cout << "Size: ";
-	cin >> size_b;
-	Vector<double> b(size_b, value_b);
-	for (size_t i = 0; i < size_b; ++i) {
-		cout << "Enter the size and value of an arbitrary vector" << endl;
-		cin >> b[i];
-	}
-	
-	
-
-	cout << "Enter size and value for the specified vector" << endl;
-	cout << "Size: ";
 	cin >> size_a;
-	while (size_a != size_b) {
-		cout << "The sizes of vectors l and a must be the same" << endl;
-		cout << "Enter size and value for the specified vector" << endl;
-		cout << "Size: ";
-		cin >> size_a;
-		break;
-	}
-	cout << "Value: ";
-	cin >> value_a;
 	Vector<double> a(size_a, value_a);
-
-	Vector<double> p = Perpendicular(a, b);
-	cout << "Perpendicular:" << '(' << p.Get_Dim() << ',' << p << ')' << endl;
+	for (size_t i = 0; i < size_a; ++i) {
+		cout << "Enter the size and value of an arbitrary vector" << endl;
+		cin >> a[i];
+	}
 	
+	
+    Vector<double> b = find_pu_vector(a);
+	cout << "Перпендикулярный единичный вектор: " << b << endl;
 
+	return 0;
 };
--- Lab1/Lab1.h
+++ Lab1/Lab1.h
@@ -4,29 +4,25 @@
 #include <cmath>
 #include <iostream>
 #include <random>
-#define VECTOR_H_
 
 using namespace std;
 
-
 template <typename T>
 class Vector {
-private: 
-	T* _elem;
-	size_t _size;
+private:
+    T* _elem;
+    size_t _size;
 
 public:
-
-
-	Vector(size_t size, const T& value = T()): _size(size), _elem(new T[size]) {
+    Vector(size_t size, const T& value = T()) : _size(size), _elem(new T[size]) {
         for (size_t i = 0; i < _size; ++i) {
             _elem[i] = value;
         }
     }
 
-    Vector(size_t size, T low_bound, T up_bound): _size(size), _elem(new T[size]) {
-        std::random_device rd;
-        std::mt19937 gen(rd());
+    Vector(size_t size, T low_bound, T up_bound) : _size(size), _elem(new T[size]) {
+        random_device rd;
+        mt19937 gen(rd());
 
         if constexpr (is_integral<T>::value) {
             uniform_int_distribution<T> dist(low_bound, up_bound);
@@ -45,24 +41,19 @@ public:
         }
     }
 
-
-    
-
     T& operator[](size_t index) {
         if (index >= _size) {
             throw out_of_range("Index out of range");
         }
         return _elem[index];
     }
-   
+
     const T& operator[](size_t index) const {
         if (index >= _size) {
             throw out_of_range("Index out of range");
         }
         return _elem[index];
     }
-
-
 
     Vector operator+(const Vector& other) const {
         if (_size != other._size) {
@@ -86,16 +77,13 @@ public:
         return result;
     }
 
-
-
-
-    T operator*(const Vector& other) const {
+     T operator*(const Vector<T>& other) const {
         if (_size != other._size) {
             throw invalid_argument("Vectors must have the same dimension");
         }
-        T result = T();
+        Vector<T> result(_size);
         for (size_t i = 0; i < _size; ++i) {
-            result += _elem[i] * other._elem[i];
+            result._elem[i] = _elem[i] * other._elem[i];
         }
         return result;
     }
@@ -108,8 +96,6 @@ public:
         return result;
     }
 
-
-    
     Vector operator/(const T& scalar) const {
         if (scalar == T()) {
             throw invalid_argument("Cannot divide by zero");
@@ -121,8 +107,6 @@ public:
         return result;
     }
 
-
-
     Vector(const Vector& other) : _size(other._size), _elem(new T[other._size]) {
         for (size_t i = 0; i < _size; ++i) {
             _elem[i] = other._elem[i];
@@ -131,9 +115,6 @@ public:
 
     ~Vector() { delete[] _elem; }
 
-
-
-	
     Vector& operator=(const Vector& other) {
         if (this != &other) {
             if (_size != other._size) {
@@ -151,52 +132,52 @@ public:
 
     size_t Get_Dim() const { return _size; }
 
-
-
-
     bool operator==(const Vector& other) const {
-        if (_size != other._size) {
+        const double eps = 1.0E-5;
+        if (abs(_size - other._size) > eps) {
             return false;
         }
 
         for (size_t i = 0; i < _size; ++i) {
-            if (_elem[i] != other._elem[i]) {
+            if (abs(_elem[i] - other._elem[i]) > eps) {
                 return false;
             }
         }
 
         return true;
     }
-    
+
     bool operator!=(const Vector& other) const { return !(*this == other); }
 
     friend ostream& operator<<(ostream& os, const Vector& vector) {
-         for (size_t i = 0; i < vector._size; ++i) {
+        for (size_t i = 0; i < vector._size; ++i) {
             os << vector._elem[i];
             if (i != vector._size - 1) {
-               
+                os << " ";
             }
         }
-        
+
         return os;
     }
-
-
 };
 
 
 template <typename T>
- double Perpendicular(const Vector<T>& a, const Vector<T>& b) {
-     T product = a * b;
-     T module = abs(a * b);
+Vector<T> find_pu_vector(const Vector<T>& a) {
+    // Нормализация вектора a
+    Vector<T> a_norm = a / sqrt(a * a);
 
-    if (module == T()) {
-        throw invalid_argument("Cannot calculate with a zero vector");
-    }
+    // Генерация случайного вектора b
+    Vector<T> b(a.Get_Dim(), T(-1), T(1));
 
-    double result = product/module;
-    return  result;
+    // Вычисление проекции вектора b на вектор a_norm
+    Vector<T> proj = (b * a_norm) * a_norm;
+
+    // Вычисление ортогонального вектора b_orth
+    Vector<T> b_orth = b - proj;
+
+    // Нормализация вектора b_orth
+    Vector<T> b_orth_norm = b_orth / sqrt(b_orth * b_orth);
+
+    return b_orth_norm;
 }
-
-
- 
