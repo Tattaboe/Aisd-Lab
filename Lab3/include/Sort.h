a20face490eef05844798889db02bd50a671f531
polina <polina@FE60>
2023-12-18 00:40:10 +0400

    real commit, bubblesort

---
+++ Lab3/include/Sort.h
@@ -0,0 +1,25 @@
+﻿
+#include "Stats.h"
+
+
+
+Stats Bubble_Sort(std::vector<int>& value)
+{
+	Stats res;
+	for (size_t i = 0; i < value.size() - 1; i++)
+	{
+		for (size_t j = 0; j < value.size() - 1 - i; j++)
+		{
+			res.comparison_count++;
+			if (value[j] > value[j + 1])
+			{
+				int tmp = value[j];
+				value[j] = value[j + 1];
+				value[j + 1] = tmp;
+				res.copy_count++;
+			}
+		}
+	}
+	return res;
+}
+
---
+++ Lab3/include/Stats.h
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+struct Stats {
+    size_t comparison_count;
+    size_t copy_count;
+
+    Stats() : comparison_count(0), copy_count(0) {}
+
+    Stats(const Stats& other)
+        : comparison_count(other.comparison_count), copy_count(other.copy_count) {}
+
+    Stats& operator=(const Stats& other) {
+        if (this != &other) {
+            comparison_count = other.comparison_count;
+            copy_count = other.copy_count;
+        }
+        return *this;
+    }
+
+    Stats& operator+=(const Stats& other) {
+        comparison_count += other.comparison_count;
+        copy_count += other.copy_count;
+        return *this;
+    }
+};
+
+std::ostream& operator<<(std::ostream& stream, const Stats& stats) {
+    stream << "Comparison count: " << stats.comparison_count << " " << "Copy count: " << stats.copy_count << std::endl;
+    return stream;
+}
